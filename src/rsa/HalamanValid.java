3c09725d62bfecedbaaedf3423a6340f2ef56fec
Shania <shaniaslsbl@gmail.com>
2023-06-08 22:26:11 +0700

    -

--- src/rsa/HalamanValid.java
+++ src/rsa/HalamanValid.java
@@ -8,12 +8,12 @@ package rsa;
  *
  * @author Shania
  */
-public class Valid extends javax.swing.JFrame {
+public class HalamanValid extends javax.swing.JFrame {
 
     /**
      * Creates new form Valid
      */
-    public Valid() {
+    public HalamanValid() {
         initComponents();
     }
 
@@ -121,20 +121,21 @@ public class Valid extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Valid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanValid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Valid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanValid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Valid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanValid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Valid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanValid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Valid().setVisible(true);
+                new HalamanValid().setVisible(true);
             }
         });
     }
